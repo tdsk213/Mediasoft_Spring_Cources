82c954856b144545335c7e892c9c49f99ae5537d
Pickle Ryskin <tdsk213@ya.ru>
2021-09-27 15:28:29 +0400

    2nd commit test

---
+++ src/main/java/homework_L2/ab_package/A.java
@@ -0,0 +1,9 @@
+package homework_L2.ab_package;
+
+
+public class A {
+
+    public void printHello() {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/homework_L2/ab_package/B.java
@@ -0,0 +1,8 @@
+package homework_L2.ab_package;
+
+public class B {
+
+    public B(A a) {
+        a.printHello();
+    }
+}
